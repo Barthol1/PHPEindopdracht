c86bbab60c26fcdcbf087e28ab1293ff18a8c42d
Barthol1 <83116011+Barthol1@users.noreply.github.com>
2022-04-02 17:39:13 +0200

    Feat/login (#1)
    * Login scaffold
    
    * Login+registratie af
    
    * Login en registratie aangepast
    
    * Fix

--- PHPEindopdracht/database/migrations/2014_10_12_000000_create_users_table.php
+++ PHPEindopdracht/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('adres');
+            $table->string('plaats');
+            $table->string('postcode');
             $table->rememberToken();
             $table->timestamps();
         });
