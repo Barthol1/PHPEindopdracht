f460cc697f30e85c506d70a3070372e05887a403
Barthol1 <bt.hol@student.avans.nl>
2022-04-02 15:49:10 +0200

    Fix

--- PHPEindopdracht/resources/views/auth/login.blade.php
+++ PHPEindopdracht/resources/views/auth/login.blade.php
@@ -43,9 +43,12 @@
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
+                        {{ __('Wachtwoord vergeten?') }}
                     </a>
                 @endif
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 ml-4" href="/register">
+                        {{ __('Account Registreren') }}
+                </a>
 
                 <x-button class="ml-3">
                     {{ __('Log in') }}
--- PHPEindopdracht/resources/views/auth/register.blade.php
+++ PHPEindopdracht/resources/views/auth/register.blade.php
@@ -68,11 +68,11 @@
 
             <div class="flex items-center justify-end mt-4">
                 <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
+                    {{ __('Al geregistreerd?') }}
                 </a>
 
                 <x-button class="ml-4">
-                    {{ __('Register') }}
+                    {{ __('Registreer') }}
                 </x-button>
             </div>
         </form>
