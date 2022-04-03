990dd30ce61140bcccac4fda9ccd05e952242e9e
Martijn-V1 <82821300+Martijn-V1@users.noreply.github.com>
2022-04-03 14:41:45 +0200

    Created roles and permissions for users
    Created roles and permissions for users (using Spatie package), created seed data RolesAndPermissionsSeeder, added middleware in Kernel.php
    
    roles:
    - superadmin
    - administratief medewerker
    - pakket inpakker
    
    permissions:
    - lezen
    - schrijven

---
+++ PHPEindopdracht/database/seeders/RolesAndPermissionsSeeder.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
+
+class RolesAndPermissionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
+        $read = Permission::create(['name' => 'lezen']);
+        $write = Permission::create(['name' => 'schrijven']);
+
+        $superadmin = Role::create(['name' => 'superadmin']);
+
+        $admin = Role::create(['name' => 'administratief medewerker']);
+        $admin->givePermissionTo($read, $write);
+
+        $packagePacker = Role::create(['name' => 'pakket inpakker']);
+        $packagePacker->givePermissionTo($read);
+
+        $user = User::factory()->create([
+            'name' => 'superadmin',
+            'email' => 'superadmin@gmail.com',
+            'password' => bcrypt('superadmin'),
+            'adres' => str::random(10),
+            'plaats'=> str::random(10),
+            'postcode' => str::random(4)
+        ]);
+        $user->assignRole($superadmin);
+
+        $user = User::factory()->create([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('admin'),
+            'adres' => str::random(10),
+            'plaats'=> str::random(10),
+            'postcode' => str::random(4)
+        ]);
+        $user->assignRole($admin);
+
+        $user = User::factory()->create([
+            'name' => 'package packer',
+            'email' => 'packagepacker@gmail.com',
+            'password' => bcrypt('packagepacker'),
+            'adres' => str::random(10),
+            'plaats'=> str::random(10),
+            'postcode' => str::random(4)
+        ]);
+        $user->assignRole($packagePacker);
+    }
+}
