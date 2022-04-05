8690dea2172db598ef41cf90d5b8e8586d1b384d
Martijn-V1 <82821300+Martijn-V1@users.noreply.github.com>
2022-04-05 18:26:55 +0200

    Bug fix
    Dashboard not found
    Added admindashboard to navigation

--- PHPEindopdracht/resources/views/layouts/navigation.blade.php
+++ PHPEindopdracht/resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Home') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('admindashboard.index')" :active="request()->routeIs('admindashboard.index')">
+                        {{ __('Admindashboard') }}
+                    </x-nav-link>
                 </div>
             </div>
 
