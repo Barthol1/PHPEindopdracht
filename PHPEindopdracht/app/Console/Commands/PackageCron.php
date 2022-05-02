448ebde7ea47f4b900aa61c1fdf28b32b72d3a17
Your Name <82821300+Martijn-V1@users.noreply.github.com>
2022-05-02 15:01:55 +0200

    Update PackageCron.php
    Na 2 dagen sorteercentrum gaat de status op verzonden

--- PHPEindopdracht/app/Console/Commands/PackageCron.php
+++ PHPEindopdracht/app/Console/Commands/PackageCron.php
@@ -36,7 +36,7 @@ class PackageCron extends Command
 
     public function handle()
     {
-        $packages = Package::where('pick_up_time', '<=', Carbon::now());
+        $packages = Package::where('pick_up_time', '<=', Carbon::now()->addDays(2));
 
         $packages->each(function($p) {
             $p->status = PackageStatus::VERZONDEN;
