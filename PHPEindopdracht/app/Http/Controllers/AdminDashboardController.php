531cc4b4138ea52a1b80ff326a3a61d1718e5e5d
Martijn-V1 <82821300+Martijn-V1@users.noreply.github.com>
2022-05-23 17:18:19 +0200

    Removed unused code

--- PHPEindopdracht/app/Http/Controllers/AdminDashboardController.php
+++ PHPEindopdracht/app/Http/Controllers/AdminDashboardController.php
@@ -137,55 +137,6 @@ class AdminDashboardController extends Controller
         return view('admindashboard.index', compact('clients', 'packages', 'webshops', 'status', 'sorting'));
     }
 
-    public function webshopstore(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        // User::create($request->all());
-        // return redirect('/admindashboard');
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
     /**
      * Update the specified resource in storage.
      *
@@ -222,16 +173,5 @@ class AdminDashboardController extends Controller
         }
 
         return redirect('/admindashboard');
- }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
